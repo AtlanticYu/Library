9a02f8b12d0c05f750466a4bd7257a0bd1eec127
WWB <s778877s@qq.com>
2018-11-13 08:37:24 -0800

    i 123

---
+++ userdialog.cpp
@@ -0,0 +1,14 @@
+#include "userdialog.h"
+#include "ui_userdialog.h"
+
+UserDialog::UserDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::UserDialog)
+{
+    ui->setupUi(this);
+}
+
+UserDialog::~UserDialog()
+{
+    delete ui;
+}
---
+++ userdialog.h
@@ -0,0 +1,22 @@
+#ifndef USERDIALOG_H
+#define USERDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class UserDialog;
+}
+
+class UserDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit UserDialog(QWidget *parent = nullptr);
+    ~UserDialog();
+
+private:
+    Ui::UserDialog *ui;
+};
+
+#endif // USERDIALOG_H
