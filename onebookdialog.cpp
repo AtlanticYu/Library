6665fe9aa00135ad51cafbbf9ee15503c420d85b
AtlanticYu <869631951.com>
2018-11-19 01:27:21 -0800

    解决了图书无法修改的问题，sql汉字执行需转码的问题

--- onebookdialog.cpp
+++ onebookdialog.cpp
@@ -61,4 +61,27 @@ void OneBookDialog::on_CbxAllPress_currentIndexChanged(const QString &arg1)
 //当ComboBox改变值时到数据库匹配
 void OneBookDialog::FillPressId()
 {
+    sqlite3 *pDb;
+    QString press_name = ui->CbxAllPress->currentText();
+    //因为初始化每次都会激活相应的槽，为了避免数据库的同时打开
+    if(InitStatus == 1)
+    {
+        if(sqlite3_open("./MyLibrary.db",&pDb) == SQLITE_OK)
+        {
+            QString sql = "select * from T_PRESS where press_name = '"+press_name+"'";
+            sqlite3_stmt *pStmt;
+            //查询中文要转成utf8，其中第二、三个参数都需要做相应修改，不转码会查询失败
+            if(sqlite3_prepare(pDb,sql.toUtf8().data(),sql.toUtf8().length(),&pStmt,NULL) == SQLITE_OK)
+//            if(sqlite3_prepare(pDb,sql.toStdString().c_str(),sql.length(),&pStmt,NULL) == SQLITE_OK)
+            {
+               while(sqlite3_step(pStmt) == SQLITE_ROW)
+               {
+                    const unsigned char *p_press_id = sqlite3_column_text(pStmt,0);
+                    QString press_id = (char *)p_press_id;
+                    ui->EdtPressId->setText(press_id);
+               }
+            }
+            sqlite3_close(pDb);
+        }
+    }
 }
